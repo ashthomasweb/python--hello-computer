3d5b427c847d9371b1ee1a7d2af2928c6cd51d4b
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-21 22:16:36 -0400

    WIP - Creates basic inputs and text display
    Uses tkinter.simpledialoge to get string and pass it to f-string
    literal. Need basic callback syntax, window layout, database entry and
    display.

--- index.py
+++ index.py
@@ -1,16 +1,55 @@
 #!/usr/bin/python
 
-import tkinter
+# import tkinter
+from tkinter import *
+
 import tkinter.messagebox
-top = tkinter.Tk()
+import tkinter.simpledialog
+
+
+# from tkinter import *
+# from tkinter import ttk
+
+gui = tkinter.Tk()
 # Code to add widgets will go here...
 
-def helloCallBack():
-   tkinter.messagebox.showinfo( "Hello Python", "Hello World")
+user = ""
 
-B = tkinter.Button(top, text ="Hello", command = helloCallBack)
+def helloCallBack():
+   global user
+   tkinter.messagebox.showinfo( "Hello Python", "Hello World")
+#    hello.pack()
+#    hello=Label(gui, text= f"Hi {user}, I'm Python. Please enter a value below:")
+   
+   C = tkinter.simpledialog.askstring( "Question", "What's your name? Type Below" )
+   user = C
+   print(C)
+   fireName()
+
+def fireName():
+   nameCallBack()
+   
+
+
+def nameCallBack():
+    global user
+    nameHello=Label(gui, text= f"Hi {user}, I'm Python. Please enter a value below:")
+    nameHello.pack()
+    D = tkinter.Entry(gui, width= 80 )
+    # printValue(D) # WIP
+    D.pack()
+
+
+# def printValue(arg):
+#     print(arg)
+
+
+
+B = tkinter.Button(gui, text ="Hello", command = helloCallBack)
 
 B.pack()
-top.mainloop()
+
+gui.geometry("800x600")
+gui.mainloop()
 
 # END of document
