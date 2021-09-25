4f3f3fb84593ae1b5770db55a552eb753ed85b9f
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-25 00:15:55 -0400

    WIP - Fixing callbacks for second frame
    OO frame needs creation.

--- index.py
+++ index.py
@@ -23,31 +23,29 @@ def hello_call_back():
 
 def nameCallBack():
     global user
-    nameHello=Label(gui, text= f"Hi {user}, I'm Python. Please enter a value below:")
-    nameHello.pack()
-    D = tkinter.Entry(gui, width= 40 )
-    D.pack()
+    nameHello=Label(greeting_frame, text= f"Hi {user}, I'm Python. Please enter a value below:")
+    nameHello.place(x=20, y=50)
+    D = tkinter.Entry(greeting_frame, width= 40 )
+    D.place(x=20, y=70)
+    response_frame = ttk.Frame(gui, width= 300, height= 200)
 
     def takeInput(entryInput):
-       print(entryInput.get())
-       userInput = entryInput.get()
-       displayInput=Text(response_frame)
-       displayInput.pack()
-       displayInput.insert('1.0', f'{user}, you entered: {userInput}')
+        print(entryInput.get())
+        userInput = entryInput.get()
+        displayInput=Text(response_frame)
 
     def inputDriver():
-       takeInput(D)
-       makeNewFrame()
+        takeInput(D)
+        makeNewFrame()
 
     def makeNewFrame():
-       response_frame = ttk.Frame(gui, width= 300, height= 200)
-       response_frame['relief'] = 'sunken'
-       response_frame.place(x= 20, y= 250)   
-
-
-
-    E = tkinter.Button(response_frame, text ='Ok', command = inputDriver)
-    E.pack()
+        response_frame['relief'] = 'sunken'
+        response_frame.place(x= 20, y= 250)   
+        displayInput.insert('1.0', f'{user}, you entered: {userInput}')
+        displayInput.pack()
+        
+    E = tkinter.Button(greeting_frame, text ='Ok', command = inputDriver)
+    E.place(x=20, y=90)
        
 
 
@@ -56,6 +54,7 @@ def nameCallBack():
 greeting_frame = ttk.Frame(gui, width= 300, height= 200)
 greeting_frame['relief'] = 'sunken'
 greeting_frame.place(x= 20, y= 50)
+greeting_frame.pack_propagate(False)
 
 # Initial 'Hello" button and beginning of name input callback chain
 B = tkinter.Button(greeting_frame, text ='Hello', command = hello_call_back)
