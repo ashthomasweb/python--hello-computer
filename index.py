338b8fd2017a3674864590205423bce4f971f595
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-27 22:55:48 -0400

    Completes 'hello' stage of project
    Fully accepts user input. Requests name and user entry. Displays user
    entry in response frame.

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 
 # import tkinter
 from tkinter import *
@@ -12,66 +11,78 @@ gui = tkinter.Tk()
 # empty string for user's name
 user = ''
 
-
+# prompt user for their name
 def hello_call_back():
    global user
    tkinter.messagebox.showinfo( 'Hello World!', 'Thanks for coming!')
-   user = tkinter.simpledialog.askstring( 'Question', "What's your name? Type Below:" )
-   print(user)
+   user = tkinter.simpledialog.askstring( 'Query:', "What's your name? Type Below:               " )
    name_call_back()
 
-
+# disable hello method, prompt for entry, display in new frame
 def name_call_back():
+    # make available variable
     global user
-    name_hello=Label(greeting_frame, text= f"Hi {user}, I'm Python. Please enter a value below:")
-    name_hello.place(x=20, y=50)
+
+    # disable initial "hello" button
+    Begin['state'] = tkinter.DISABLED
+    
+    # create label for user entry
+    enter_value=Label(greeting_frame, text= f"Hi {user}, I am Python. Please enter a value below:")
+    enter_value.place(x=20, y=50)
+
+    # create user entry field
     D = tkinter.Entry(greeting_frame, width= 40 )
     D.place(x=20, y=70)
-    response_frame = ttk.Frame(gui, width= 300, height= 200)
+
+    # create text field
     display_input=Text(response_frame)
-    user_input = ""
 
-    def take_input(entry_input):
-        print(entry_input.get())
-        user_input = entry_input.get()
-        print(user_input)
-    
-    print(user_input)
-
+    # empty string for user input
+    user_input = ''
+   
+    # gets user entered value and sends it to response frame
     def input_driver():
-        take_input(D)
+        user_input = D.get()
         make_new_frame(user_input)
 
+    # builds response frame and inserts user entered value
     def make_new_frame(input):
-        print(input)
-
-        response_frame['relief'] = 'sunken'
-        response_frame.place(x= 20, y= 250)   
-        display_input.insert('1.0', f'{user}, you entered: {input}')
+        response_frame.place(x= 20, y= 230)   
+        display_input.insert('1.0', f'{user}, you entered: {input}\n')
         display_input.pack()
-        
+    
+    # displays entered string in response_frame
     E = tkinter.Button(greeting_frame, text ='Ok', command = input_driver)
     E.place(x=20, y=90)
-       
-# user_input not passing correctly - WIP
 
+# layout frames
+hello_frame = ttk.Frame(gui, width= 340, height= 460)
+hello_frame['relief'] = 'raised'
+hello_frame.place(x= 230, y= 20)
+hello_frame.pack_propagate(False)
 
-
-greeting_frame = ttk.Frame(gui, width= 300, height= 200)
+greeting_frame = ttk.Frame(hello_frame, width= 300, height= 200)
 greeting_frame['relief'] = 'sunken'
-greeting_frame.place(x= 20, y= 50)
+greeting_frame.place(x= 20, y= 20)
 greeting_frame.pack_propagate(False)
 
+response_frame = ttk.Frame(hello_frame, width= 300, height= 200)
+response_frame['relief'] = 'groove'
+response_frame.pack_propagate(False)
+
 # Initial 'Hello" button and beginning of name input callback chain
-B = tkinter.Button(greeting_frame, text ='Hello', command = hello_call_back)
-B.place(x=20, y=10)
+Begin = tkinter.Button(greeting_frame, text ='Hello', command = hello_call_back)
+Begin.place(x=20, y=10)
 
+# Calculator
 
+# Database Operations
 
-
-
+# main window styling options
 gui.title('Hello, Meta... Python Desktop Application')
 gui.geometry('800x600')
+
+# run program
 gui.mainloop()
 
 # END of document
