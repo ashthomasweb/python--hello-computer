106d866fdc04d4bd23900beff2bbceb95c59031e
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-30 22:09:46 -0400

    WIP - creation of OO frames
    Having issues creating frames within frames as variable is not assigned?
    New language woes.

---
+++ oopractice.py
@@ -0,0 +1,27 @@
+
+
+class Fruit(input):
+    def color(self, color):
+        self.color = color
+
+    def name(self, name):
+        self.name = name
+
+    def shape(self, shape):
+        self.shape = shape
+
+    def edible(self, input)
+        self.edible = input
+
+    def output(self):
+        print(self.color, self.name, self.shape, self.edible)
+
+apple = Fruit('true')
+
+apple.color('red')
+apple.name('Fuji')
+apple.shape('roundish')
+apple.output()
+
+
+
